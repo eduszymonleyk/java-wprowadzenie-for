996d62a9f314673723c4250de76c1eadbc3a5eb2
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-29 10:21:38 +0100

    #1 wyświetlenie liczb od 91 do -7

---
+++ src/DisplayNumbers.java
@@ -0,0 +1,13 @@
+public class DisplayNumbers {
+
+    public static void main(String[] args) {
+        // zadanie 1 od 91 do -7
+//        System.out.println(91);
+//        System.out.println(90);
+//        System.out.println(89);
+
+        for (int i = 91; i >= -7; i--) {
+            System.out.println(i);
+        }
+    }
+}
