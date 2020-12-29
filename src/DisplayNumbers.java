85e7f8445029035c52c72f51a0ac564af936191d
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-29 10:26:50 +0100

    #3 zadanie2, wyświetlenie parzystych liczb z zakresu

--- src/DisplayNumbers.java
+++ src/DisplayNumbers.java
@@ -1,6 +1,8 @@
 public class DisplayNumbers {
 
     public static void main(String[] args) {
+
+        System.out.println("----------- zadanie 1 ------------");
         // zadanie 1 od 91 do -7
 //        System.out.println(91);
 //        System.out.println(90);
@@ -8,6 +10,15 @@ public class DisplayNumbers {
 
         for (int i = 91; i >= -7; i--) {
             System.out.println(i);
+        }
+
+        System.out.println("----------- zadanie 2 ------------");
+
+        // zadanie 2
+        for(int i = 62349; i <= 62392;i++){
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
